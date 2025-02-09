3e3aaa4497afb79bf75cbb48ba8adbfe24ab7ce8
Daiki007121 <dkoike@mail.sfsu.edu>
2025-02-08 16:55:15 -0800

    update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './db/mongodb.js';
 import spotsRouter from './routes/spots.js';
 import reviewsRouter from './routes/reviews.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -12,7 +17,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/spots', spotsRouter);
 app.use('/api/reviews', reviewsRouter);
